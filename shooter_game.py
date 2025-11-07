342554d321f3edae39f90c8b1e2f4754df4e4c24
nigaganksta5-dev <nigaganksta5@gmail.com>
2025-11-07 17:30:19 +0500

    v1
    Базовая версия

--- shooter_game.py
+++ shooter_game.py
@@ -10,9 +10,9 @@ mixer.init()
 # mixer.music.set_volume(0.3)
 # shoot = mixer.Sound('fire.ogg')
 #создай окно игры
-window = display.set_mode((800,400))
-display.set_caption('Пинг-понг')
-background = transform.scale(image.load('phon.jpg'),(800,400))
+window = display.set_mode((500,900))
+display.set_caption('Шутер')
+background = transform.scale(image.load('phon.jpg'),(500,900))
 y1 = 350
 y2 = 350
 x1 = 100
@@ -36,23 +36,18 @@ class Gamesprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(Gamesprite):
-    def update_l(self):
+    def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 350:
-            self.rect.y += self.speed
-    def update_r(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 350:
-            self.rect.y += self.speed
+        if keys_pressed[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] and self.rect.x < 420:
+            self.rect.x += self.speed
+    
+    
+player = Player('Hero.png.png', 250,850, 10,80,50)
 
-    
-    
-player_l = Player('Hero.png.png', 0,200, 10,80,50)
-player_r = Player('Hero2.png', 720,200, 10, 80, 50)
+num_fire = 0
+rel_time = False
 
 clock = time.Clock()
 FPS = 60
@@ -64,14 +59,21 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                if num_fire <= 5 and rel_time == False:
+                    player.fire()
+                    shoot.play() 
+                    num_fire += 1
+                if num_fire > 5 and rel_time == False:
+                    rel_time = True
+                    start = tm()
                         
     if finish != True:
         window.blit(background,(0,0))
-        player_l.update_l()
-        player_l.reset()
-        player_r.update_r()
-        player_r.reset()
+        player.update()
+        player.reset()
+        
 
     display.update()
     clock.tick(FPS)
