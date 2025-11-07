e3db54c70be8736e792d43b31d5c7eb693dead83
nigaganksta5-dev <nigaganksta5@gmail.com>
2025-11-07 17:11:40 +0500

    v1
    Первоначальная заготовка игры. Добавлен фон.

---
+++ shooter_game.py
@@ -0,0 +1,68 @@
+#Создай собственный Шутер!
+from pygame import *
+from random import * 
+from time import time as tm
+
+font.init()
+mixer.init()
+# mixer.music.load('space.ogg')
+# mixer.music.play()
+# mixer.music.set_volume(0.3)
+# shoot = mixer.Sound('fire.ogg')
+#создай окно игры
+window = display.set_mode((800,400))
+display.set_caption('Пинг-понг')
+background = transform.scale(image.load('phon.jpg'),(800,400))
+y1 = 350
+y2 = 350
+x1 = 100
+x2 = 300
+font0 = font.SysFont('Arial', 40)
+win = font0.render('Ты убил всех хлебом', True, (255,215, 0))
+loss = font0.render('Ты потратил слишком много хлеба', True,(255,215,0))
+font1 = font.SysFont('Arial',20)
+points = 0
+loser = 0
+
+class Gamesprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y,player_speed,w,h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w,h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(Gamesprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] and self.rect.x < 420:
+            self.rect.x += self.speed
+    
+    
+player = Player('Hero.png.png', 250,850, 10,80,50)
+
+clock = time.Clock()
+FPS = 60
+speed = 10
+game = True
+finish = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        
+                        
+    if finish != True:
+        window.blit(background,(0,0))
+        player.update()
+        player.reset()
+        
+
+    display.update()
+    clock.tick(FPS)
